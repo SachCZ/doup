5ef10e7b7d240ee830f0b16a7d31330346be7090
Martin Šach <martin.sach@firma.seznam.cz>
2022-10-30 21:14:58 +0000

    Improve install script

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
-USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
-cp -r workspace "${USER_HOME}/.doupworkspace"
-cp doup.py /usr/local/bin/doup
-chmod +x /usr/local/bin/doup
-cp doup-completion /etc/bash_completion.d/doup
+WORKSPACE_DIR=${HOME}/.doupworkspace
+echo "Installing templates to ${WORKSPACE_DIR}"
+mkdir -p ${WORKSPACE_DIR}
+cp -r workspace/* ${WORKSPACE_DIR}
+echo "Installing exe to /usr/local/bin/doup"
+sudo cp doup.py /usr/local/bin/doup
+sudo chmod +x /usr/local/bin/doup
+echo "Installing completion to /etc/bash_completion.d/doup"
+sudo cp doup-completion /etc/bash_completion.d/doup
 echo "Done."
-
